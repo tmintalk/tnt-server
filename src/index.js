dba5eb529337f63c1ed13cb13b8e034f2c6144fe
T-dubb <rlaxodntttt@kaist.ac.kr>
2021-10-26 04:10:40 +0900

    feat: simple open chatting

--- src/index.js
+++ src/index.js
@@ -1,47 +1,63 @@
-const express = require('express');
-const morgan = require('morgan');
-const passport = require('passport');
-const helmet = require('helmet');
-const cors = require('cors');
+const express = require("express");
+const morgan = require("morgan");
+const passport = require("passport");
+const helmet = require("helmet");
+const cors = require("cors");
+const socketIO = require("socket.io");
+var http = require("http").createServer(express);
 
-require('dotenv').config();
-const { sequelize } = require('./models');
+require("dotenv").config();
+const { sequelize } = require("./models");
 
-const rootRoutes = require('./routes');
+const rootRoutes = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-sequelize.sync({ force: false })
+sequelize
+  .sync({ force: false })
   .then(() => {
-    console.log('데이터베이스 연결 성공');
+    console.log("데이터베이스 연결 성공");
   })
   .catch((err) => {
     console.error(err);
   });
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(passport.initialize());
 app.use(cors());
 
-require('./config/passport')(passport);
+require("./config/passport")(passport);
 
-app.use('/', rootRoutes);
+app.use("/", rootRoutes);
 
 app.use((req, res, next) => {
-  const error =  new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
   error.status = 404;
   next(error);
 });
 
 app.use((err, req, res, next) => {
   res.locals.message = err.message;
-  res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+  res.locals.error = process.env.NODE_ENV !== "production" ? err : {};
   res.status(err.status || 500);
   res.status(404).send(err);
+});
+// Socket 연결
+const io = socketIO(http, {
+  cors: {
+    origin: "*",
+    credentials: true,
+  },
+});
+io.on("connection", (socket) => {
+  console.log("a user connected!");
+  socket.on("message", ({ name, message }) => {
+    io.emit("message", { name, message });
+  });
 });
 
 app.listen(PORT, () => {

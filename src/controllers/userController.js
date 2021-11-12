46f3aa07fc141b2e453b3746565314045194deae
thestarkor <sk_and_mc@kaist.ac.kr>
2021-11-13 04:43:51 +0900

    [feat] 유저 정보 api 추가

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,9 +1,33 @@
-const { User } = require("../models")
+const { User, Post, Question, Answer } = require("../models")
 
 module.exports = {
   getUsers: async (req, res, next) => {
     const users = await User.findAll();
 
     res.json(users);
+  },
+  getUser: async (req, res, next) => {
+    const { userId } = req.params;
+
+    console.log(userId);
+    const user = await User.findOne({
+      where: {
+        id: userId
+      },
+      include: [
+        {
+          model: Post,
+          include: [{ model: User }]
+        },
+        {
+          model: Answer,
+          include: [{ model: Question }]
+        }
+      ]
+    });
+
+    console.log(user);
+
+    res.json(user);
   }
 }
--- src/routes/users.js
+++ src/routes/users.js
@@ -5,6 +5,7 @@ const userController = require('../controllers/userController');
 
 const router = express.Router();
 
+router.get('/:userId', isLoggedIn, userController.getUser);
 router.get('/', isLoggedIn, userController.getUsers);
 
 module.exports = router;
